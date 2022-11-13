e27cf2b7097c9d1a86e79b3108cf5d8cfb7b978c
Gopikrisna <117550448+Gopikrisna@users.noreply.github.com>
2022-11-13 09:05:39 +0530

    Add files via upload

---
+++ week2/day1/SeleniumBasics.java
@@ -0,0 +1,41 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumBasics 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Sample test Leaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Gopi");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Krisna");
+		WebElement sourcedd = driver.findElement(By.id("createLeadForm_dataSourceId"));
+		Select sourceddSelect = new Select(sourcedd);
+		sourceddSelect.selectByValue("LEAD_PARTNER");
+		driver.findElement(By.name("submitButton")).click();
+		
+		driver.close();
+		
+	}
+
+}
